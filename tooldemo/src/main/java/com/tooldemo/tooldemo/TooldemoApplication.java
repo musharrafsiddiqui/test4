13e0a9a1d98562ae41e4d5170c2e1dcf4daff7d3
musharrafsiddiqui <musharrafsid313@gmail.com>
2025-05-09 05:47:40 +0530

    first commit

---
+++ tooldemo/src/main/java/com/tooldemo/tooldemo/TooldemoApplication.java
@@ -0,0 +1,13 @@
+package com.tooldemo.tooldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TooldemoApplication.class, args);
+	}
+
+}
---
+++ tooldemo/src/test/java/com/tooldemo/tooldemo/TooldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tooldemo.tooldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
